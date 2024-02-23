2bf8b545e602de038272a74342a1b643de40a4c9
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2024-02-23 14:41:51 +0700

    add login, logout with cookie

--- SelfDrivingCarRentalPlatform/Program.cs
+++ SelfDrivingCarRentalPlatform/Program.cs
@@ -8,19 +8,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<SdcrpDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("SdcrpConnection"));
-    options.UseNpgsql(builder => builder.MigrationsAssembly("SelfDrivingCarRentalPlatform"));
+	options.UseNpgsql(builder.Configuration.GetConnectionString("SdcrpConnection"));
+	options.UseNpgsql(builder => builder.MigrationsAssembly("SelfDrivingCarRentalPlatform"));
 }, ServiceLifetime.Singleton);
 builder.Services.InjectServices(); // add service for DI
+
+//add cookie authentication
+builder.Services.AddAuthentication("LoginCookieAuth")
+	.AddCookie("LoginCookieAuth", options =>
+	{
+		options.Cookie.Name = "LoginCookieAuth";
+		options.LoginPath = "/Account/Login";
+	});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -28,6 +36,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
