cb5a1c9ccad6293c24647f6722818a14bab43765
walle <buihoanghai2405@gmail.com>
2024-03-11 21:11:34 +0700

    cancelDate + contractStatus

---
+++ BusinessObjects/Enums/ContractStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Enums
+{
+    public enum ContractStatus : int
+    {
+        Signed = 0,
+        Received = 1,
+        Returned = 2
+    }
+}
---
+++ SelfDrivingCarRentalPlatform/Pages/Contracts/Cancel.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Models;
+using DAOs;
+using Repositories.Interfaces;
+
+namespace SelfDrivingCarRentalPlatform.Pages.Contracts
+{
+    public class CancelModel : PageModel
+    {
+        private readonly IContractRepository _contractRepository;
+        private readonly ITransactionRepository _transactionRepository;
+
+        public CancelModel(IContractRepository contractRepository, ITransactionRepository transactionRepository)
+        {
+            _contractRepository = contractRepository;
+            _transactionRepository = transactionRepository;
+        }
+
+        [BindProperty]
+        public Contract Contract { get; set; } = default!;
+
+        public Transaction Transaction { get; set; }
+        
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _contractRepository.GetAll() == null)
+            {
+                return NotFound();
+            }
+
+            var contract = _contractRepository.GetAll().Where(c => c.Id == id).FirstOrDefault();
+            var transaction = _transactionRepository.GetAll().Where(c => c.Id == id).FirstOrDefault();
+
+            if (contract == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Contract = contract;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _contractRepository.GetAll() == null)
+            {
+                return NotFound();
+            }
+            var contract = _contractRepository.GetAll().Where(c => c.Id == id).FirstOrDefault();
+
+            if (contract != null)
+            {
+                contract.IsDeleted = true;
+                Contract = contract;
+                _contractRepository.Update(Contract);
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
