60a8526974f3f5601f9bf3dc94ed7b7f5dc1e8c5
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2024-03-21 16:18:57 +0700

    contract detail for carOwner

---
+++ SelfDrivingCarRentalPlatform/Pages/CarOwners/Contracts/RentDetail.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using BusinessObjects.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Interfaces;
+
+namespace SelfDrivingCarRentalPlatform.Pages.CarOwners.Contracts;
+
+public class RentDetail : PageModel
+{
+    private readonly IContractRepository _contractRepository;
+
+    public RentDetail(IContractRepository contractRepository)
+    {
+        _contractRepository = contractRepository;
+    }
+    
+    public Contract Contract { get; set; } = new Contract();
+    public void OnGet(int contractId)
+    {
+        Contract = _contractRepository.GetAll()
+            .Include(c => c.Car)
+            .ThenInclude(car => car.CarOwner).ThenInclude(owner => owner.Location)
+            .Include(c => c.Car)
+            .ThenInclude(car => car.CarType)
+            .Include(c => c.Car)
+            .ThenInclude(car => car.CarBrand)
+            .Include(c => c.Transaction)
+            .FirstOrDefault(c => c.Id == contractId);
+    }
+}
