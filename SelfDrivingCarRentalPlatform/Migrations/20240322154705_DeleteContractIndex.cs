879fd6df6f4f6b78c78e43585ef8fb1c4ff2c16c
luosrevlis <tuanminhphamtdn@gmail.com>
2024-03-22 22:47:32 +0700

    db delete contract index

---
+++ SelfDrivingCarRentalPlatform/Migrations/20240322154705_DeleteContractIndex.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SelfDrivingCarRentalPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteContractIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Contracts_CarId_RentStartDate",
+                table: "Contracts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contracts_CarId",
+                table: "Contracts",
+                column: "CarId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Contracts_CarId",
+                table: "Contracts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contracts_CarId_RentStartDate",
+                table: "Contracts",
+                columns: new[] { "CarId", "RentStartDate" },
+                unique: true);
+        }
+    }
+}
